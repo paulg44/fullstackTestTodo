17009d844f3912febef6f5eaaaf60163ae64fb9b
paulg44 <paulgarton1984@hotmail.com>
2024-04-01 17:39:22 +0100

    add states and function for inut changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,11 @@ import React from "react";
 import "./App.css";
 import TodoForm from "./TodoForm/TodoForm";
 
+/* TODO 
+  - create form and add to app (done)
+  - create list display and add to app
+*/
+
 function App() {
   return (
     <div className="App">
--- client/src/TodoForm/TodoForm.tsx
+++ client/src/TodoForm/TodoForm.tsx
@@ -2,25 +2,58 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./TodoForm.css";
+import { ChangeEvent, useState } from "react";
+
+/* TODO 
+  - create form (done)
+  - add useStates for each input
+  - create function for add to list button
+*/
 
 function TodoForm() {
-  const priority = ["high", "medium", "low"];
-  const category = ["personal", "work"];
+  // Use States for form
+  const [todo, setTodo] = useState("");
+  const [priority, setPriority] = useState("");
+  const [category, setCategory] = useState("");
+
+  // Priority and Category option arrays
+  const priorityArr = ["high", "medium", "low"];
+  const categoryArr = ["personal", "work"];
+
+  function handleTodoChange(e: ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    setTodo(e.target.value);
+    console.log(todo);
+  }
+
+  // These function not working properly yet!!!
+  function handlePriorityChange(e: ChangeEvent<HTMLOptionElement>) {
+    e.preventDefault();
+    setPriority(e.target.value);
+    console.log(priority);
+  }
+  function handleCategoryChange(e: ChangeEvent<HTMLOptionElement>) {
+    e.preventDefault();
+    setCategory(e.target.value);
+    console.log(category);
+  }
 
   return (
     <Form className="todoForm">
       {/* Enter todo */}
       <Form.Group controlId="formTodo">
         <Form.Label>Enter todo description</Form.Label>
-        <Form.Control type="text"></Form.Control>
+        <Form.Control type="text" onChange={handleTodoChange}></Form.Control>
       </Form.Group>
 
       {/* Add priority */}
       <Form.Group>
         <Form.Label htmlFor="priority">Priority</Form.Label>
         <Form.Select id="priority">
-          {priority.map((pri) => (
-            <option value={pri}>{pri}</option>
+          {priorityArr.map((pri, index) => (
+            <option onChange={handlePriorityChange} key={index} value={pri}>
+              {pri}
+            </option>
           ))}
         </Form.Select>
       </Form.Group>
@@ -29,8 +62,10 @@ function TodoForm() {
       <Form.Group>
         <Form.Label htmlFor="category">Category</Form.Label>
         <Form.Select id="category">
-          {category.map((cat) => (
-            <option value={cat}>{cat}</option>
+          {categoryArr.map((cat, index) => (
+            <option onChange={handleCategoryChange} key={index} value={cat}>
+              {cat}
+            </option>
           ))}
         </Form.Select>
       </Form.Group>
